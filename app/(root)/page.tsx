66905229ce3d65cff3daae29aab53b8a58e3fc39
OtbeyNat <tant3@rpi.edu>
2024-10-30 12:48:30 -0400

    turn off caching for all posts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@/auth";
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { client } from "@/sanity/lib/client";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
@@ -12,8 +13,8 @@ export default async function Home({searchParams} : {
  
   const session = await auth();
   console.log(session?.id)
-  // const posts = await client.fetch(STARTUPS_QUERY);
-  const { data: posts } = await sanityFetch({query: STARTUPS_QUERY, params})
+  const posts = await client.withConfig({useCdn:false}).fetch(STARTUPS_QUERY,params);
+  // const { data: posts } = await sanityFetch({query: STARTUPS_QUERY, params})
 
   return (
     <>
